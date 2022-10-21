2fc0c1a51436f82bd93fd8a78112027cbbbeb980
Gustavo Lodi Vidaletti <gustavo.vidaletti@gmail.com>
2022-10-20 21:28:35 -0300

    feat: Estrutura inicial + Imagens

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div>OIOIOI</div>
   </React.StrictMode>
 );
 
@@ -23,3 +22,18 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+ * app.test.tsx
+ * // import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+export {}
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+ *  */
